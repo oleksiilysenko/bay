070082d3fb7b19eddf28b6e24044cf0215a8a406
oleksiilysenko <38806632+oleksiilysenko@users.noreply.github.com>
2022-08-28 23:00:20 +0200

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,8 +9,7 @@
 
   <button type="button" @click="state.count++">count is: {{ state.count }}</button>
   <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    Thank you.
   </p>
 </template>
 
